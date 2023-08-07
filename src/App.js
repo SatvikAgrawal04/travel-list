9e500857c5ace931e14c285183ef7706290a412b
Satvik Agrawal <satwikagrawal@gmail.com>
2023-08-07 21:09:49 +0530

    07-08-23

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import { useState } from "react";
+
+const initialItems = [
+	{ id: 1, description: "Passports", quantity: 2, packed: false },
+	{ id: 2, description: "Socks", quantity: 12, packed: true },
+	{ id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
 export default function App() {
 	return (
 		<div>
@@ -14,17 +22,68 @@ function Logo() {
 }
 
 function Form() {
+	const [description, setDescription] = useState("");
+	const [quantity, setQuantity] = useState(1);
+	function handleSubmit(e) {
+		e.preventDefault();
+		if (!description) {
+			return;
+		}
+		const newItem = { description, quantity, packed: false, id: Date.now() };
+		console.log(newItem);
+		// initialItems.push(newItem);
+
+		setDescription("");
+		setQuantity(1);
+	}
+
 	return (
-		<div className="add-form">
+		<form className="add-form" onSubmit={handleSubmit}>
 			<h3>What do you need for the trip</h3>
-		</div>
+			<select value={quantity} onChange={e => setQuantity(+e.target.value)}>
+				{Array.from({ length: 20 }, (_, i) => i + 1).map(num => (
+					<option value={num} key={num}>
+						{num}
+					</option>
+				))}
+			</select>
+			<input
+				type="text"
+				placeholder="Item..."
+				value={description}
+				onChange={e => setDescription(e.target.value)}
+			/>
+			<button>Add</button>
+		</form>
 	);
 }
 
 function PackingList() {
-	return <div className="list">List</div>;
+	return (
+		<div className="list">
+			<ul>
+				{initialItems.map(item => (
+					<Items itemObj={item} key={item.id} />
+				))}
+			</ul>
+		</div>
+	);
+}
+function Items({ itemObj }) {
+	return (
+		<li>
+			<span className={`${itemObj.packed && "strike"}`}>
+				{itemObj.quantity} {itemObj.description}
+			</span>
+			<button>❌</button>
+		</li>
+	);
 }
 
 function Stats() {
-	return <footer>You have X items on your list, and you have already </footer>;
+	return (
+		<footer className="stats">
+			<em>You have X items on your list, and you have already packed X (X%)</em>
+		</footer>
+	);
 }
